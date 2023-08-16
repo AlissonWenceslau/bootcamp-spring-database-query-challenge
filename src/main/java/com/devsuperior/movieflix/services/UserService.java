8c4c5f95b16cf2cb5369b342147129f1b0faab3d
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-08-16 02:29:27 -0300

    Services

---
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -0,0 +1,43 @@
+package com.devsuperior.movieflix.services;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService{
+	
+	@Autowired
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private AuthService authService;
+	
+	@Transactional(readOnly = true)
+	public UserDTO userCurrentLogged() {
+		User user = authService.authenticated();
+		return new UserDTO(user);
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if(user == null) {
+			logger.error("Not Fount: " + username);
+			throw new UsernameNotFoundException(" Email Not Found: " + username);
+		}
+		logger.info("Found User: " + username);
+		return user;
+	}
+}
